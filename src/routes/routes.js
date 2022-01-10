004686c2ce57b8339890ca5ad4036aa5ff060737
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 15:30:48 +0800

    fix: route get instead of use

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,6 @@ import express from 'express';
 const routes = express();
 
 routes.use(express.json());
-routes.use('/', (req, res) => res.json({ message: 'success' }));
+routes.get('/', (req, res) => res.json({ message: 'success' }));
 
 export default routes;
