4c74bb4498edf64e00d3556fe5bd322914f058ea
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 16:46:25 +0800

    fix: custom message in error

--- src/utils/customError.js
+++ src/utils/customError.js
@@ -1,7 +1,7 @@
 export const BAD_REQUEST = { status: 400, message: 'Bad Request' };
 
-export default ({ status, message }) => {
-  const error = new Error(message);
+export default ({ status, message }, customMessage) => {
+  const error = new Error(message || customMessage);
   error.status = status;
   throw error;
 };
