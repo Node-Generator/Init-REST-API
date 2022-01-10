a4307e7d346935473bc4e30fe976c6073e3990b2
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 16:20:04 +0800

    fix: linter

--- src/utils/customError.js
+++ src/utils/customError.js
@@ -2,14 +2,13 @@ export default (errorString, code = 500) => {
   const error = new Error(errorString);
   error.code = code;
   throw error;
-}
+};
 
-export const errorHandler = routes.use((error, req, res, next) => {
+export const errorHandler = (error, req, res, next) => {
   if (error) {
     return res.status(error.code).json({
       message: error.message,
     });
-  } else {
-    next();
   }
-})
+  return next();
+};
