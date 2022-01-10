4624768aeb5fda637261572a58c2512e8f6930fb
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 10:36:31 +0800

    fix: change package const to pack. Unexpected strict mode reserved word

--- initAPI.js
+++ initAPI.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const packageFile = `${process.cwd()}/package.json`;
 
 const packageRaw = fs.readFileSync(packageFile);
-const package = JSON.parse(packageRaw);
+const pack = JSON.parse(packageRaw);
 
-package.type = 'module';
+pack.type = 'module';
 
-package.scripts.lint = 'eslint ./src';
-package.scripts.dev = 'node -r dotenv/config --experimental-specifier-resolution=node src/index.js';
-package.scripts.start = 'node --experimental-specifier-resolution=node src/index.js';
+pack.scripts.lint = 'eslint ./src';
+pack.scripts.dev = 'node -r dotenv/config --experimental-specifier-resolution=node src/index.js';
+pack.scripts.start = 'node --experimental-specifier-resolution=node src/index.js';
 
-fs.writeFileSync(packageFile, JSON.stringify(package, null, 2));
+fs.writeFileSync(packageFile, JSON.stringify(pack, null, 2));
